20c3f5d0519e163d8baa8ec094e0ea01bca12deb
Patrick Walters <patrickwaltersc21@yahoo.com>
2015-06-24 08:32:18 -0500

    fix(jspm): Fixes config.js fork issue
    Fixes #63 caused by not running jspm clean after updating dependencies.

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 System.config({
-  "transpiler": "traceur",
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
@@ -14,21 +13,10 @@ System.config({
     "aurelia-templating": "github:aurelia/templating@0.12.1",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
-    "github:aurelia/binding@0.6.1": {
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
-      "aurelia-metadata": "github:aurelia/metadata@0.5.0",
-      "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
-      "core-js": "npm:core-js@0.9.15"
-    },
     "github:aurelia/binding@0.7.3": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.8.1",
       "aurelia-metadata": "github:aurelia/metadata@0.6.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.5.0",
-      "core-js": "npm:core-js@0.9.15"
-    },
-    "github:aurelia/dependency-injection@0.7.1": {
-      "aurelia-logging": "github:aurelia/logging@0.4.0",
-      "aurelia-metadata": "github:aurelia/metadata@0.5.0",
       "core-js": "npm:core-js@0.9.15"
     },
     "github:aurelia/dependency-injection@0.8.1": {
@@ -36,33 +24,13 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.6.0",
       "core-js": "npm:core-js@0.9.15"
     },
-    "github:aurelia/loader@0.6.0": {
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-      "aurelia-path": "github:aurelia/path@0.6.1",
-      "core-js": "npm:core-js@0.9.15",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
-    },
     "github:aurelia/loader@0.7.0": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
       "aurelia-path": "github:aurelia/path@0.7.0",
       "core-js": "npm:core-js@0.9.15",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
     },
-    "github:aurelia/metadata@0.5.0": {
-      "core-js": "npm:core-js@0.9.15"
-    },
     "github:aurelia/metadata@0.6.0": {
-      "core-js": "npm:core-js@0.9.15"
-    },
-    "github:aurelia/templating@0.11.2": {
-      "aurelia-binding": "github:aurelia/binding@0.6.1",
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-      "aurelia-loader": "github:aurelia/loader@0.6.0",
-      "aurelia-logging": "github:aurelia/logging@0.4.0",
-      "aurelia-metadata": "github:aurelia/metadata@0.5.0",
-      "aurelia-path": "github:aurelia/path@0.6.1",
-      "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
       "core-js": "npm:core-js@0.9.15"
     },
     "github:aurelia/templating@0.12.1": {
