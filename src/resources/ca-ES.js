36fc96c7d1ec57e1ef38012067ea984438079264
dpinart <dpinart@newuibcn.com>
2015-08-27 09:58:22 +0200

    Catalan language, ca-ES, added

---
+++ src/resources/ca-ES.js
@@ -0,0 +1,100 @@
+/**
+ * Created by dpinart on 27/08/2015.
+ */
+/* */
+define(['exports', 'aurelia-validation'], function (exports, aurelia_validation_1) {
+  'use strict';
+
+  exports.__esModule = true;
+  var data = {
+    settings: {
+      'numericRegex': /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/
+    },
+    messages: {
+      'isRequired': 'és requerit',
+      'onValidateCallback': 'no és un valor vàlid',
+      'AlphaNumericOrWhitespaceValidationRule': function AlphaNumericOrWhitespaceValidationRule(newValue, threshold) {
+        return 'només pot contenir caràcters alfanumèrics o espais';
+      },
+      'AlphaNumericValidationRule': function AlphaNumericValidationRule(newValue, threshold) {
+        return 'només pot contenir caràcters alfanumèrics';
+      },
+      'AlphaValidationRule': function AlphaValidationRule(newValue, threshold) {
+        return 'només pot contenir lletres';
+      },
+      'AlphaOrWhitespaceValidationRule': function AlphaOrWhitespaceValidationRule(newValue, threshold) {
+        return 'només pot contenir lletres o espais';
+      },
+      'BetweenLengthValidationRule': function BetweenLengthValidationRule(newValue, threshold) {
+        return 'el nombre de caràcters ha d\'estar entre ' + aurelia_validation_1.Utilities.getValue(threshold.minimumLength) + ' i ' + _validationUtilities.Utilities.getValue(threshold.maximumLength);
+      },
+      'BetweenValueValidationRule': function BetweenValueValidationRule(newValue, threshold) {
+        return 'ha d\'estar entre ' + aurelia_validation_1.Utilities.getValue(threshold.minimumValue) + ' i ' + _validationUtilities.Utilities.getValue(threshold.maximumValue);
+      },
+      'CustomFunctionValidationRule': function CustomFunctionValidationRule(newValue, threshold) {
+        return 'no és un valor vàlid';
+      },
+      'DigitValidationRule': function DigitValidationRule(newValue, threshold) {
+        return 'pot contenir només dígits';
+      },
+      'EmailValidationRule': function EmailValidationRule(newValue, threshold) {
+        return 'no és una adreça de correu electrònic vàlida';
+      },
+      'EqualityValidationRule': function EqualityValidationRule(newValue, threshold) {
+        return 'hauria de ser ' + aurelia_validation_1.Utilities.getValue(threshold.otherValue);
+      },
+      'InEqualityValidationRule': function InEqualityValidationRule(newValue, threshold) {
+        return 'no pot ser ' + aurelia_validation_1.Utilities.getValue(threshold.otherValue);
+      },
+      'EqualityWithOtherLabelValidationRule': function EqualityWithOtherLabelValidationRule(newValue, threshold) {
+        return 'no coincideix ' + aurelia_validation_1.Utilities.getValue(threshold.otherValueLabel);
+      },
+      'InEqualityWithOtherLabelValidationRule': function InEqualityWithOtherLabelValidationRule(newValue, threshold) {
+        return 'no pot coincidir ' + aurelia_validation_1.Utilities.getValue(threshold.otherValueLabel);
+      },
+      'InCollectionValidationRule': function InCollectionValidationRule(newValue, threshold) {
+        return 'no és un valor vàlid';
+      },
+      'MinimumInclusiveValueValidationRule': function MinimumInclusiveValueValidationRule(newValue, threshold) {
+        return 'ha de ser ' + aurelia_validation_1.Utilities.getValue(threshold) + ' o més';
+      },
+      'MinimumLengthValidationRule': function MinimumLengthValidationRule(newValue, threshold) {
+        return 'ha de tenir almenys ' + aurelia_validation_1.Utilities.getValue(threshold) + ' caràcters';
+      },
+      'MinimumValueValidationRule': function MinimumValueValidationRule(newValue, threshold) {
+        return 'ha de ser major que ' + aurelia_validation_1.Utilities.getValue(threshold);
+      },
+      'MaximumInclusiveValueValidationRule': function MaximumInclusiveValueValidationRule(newValue, threshold) {
+        return 'ha de ser ' + aurelia_validation_1.Utilities.getValue(threshold) + ' o menys';
+      },
+      'MaximumLengthValidationRule': function MaximumLengthValidationRule(newValue, threshold) {
+        return 'no pot ser més llarg de ' + aurelia_validation_1.Utilities.getValue(threshold) + ' caràcters';
+      },
+      'MaximumValueValidationRule': function MaximumValueValidationRule(newValue, threshold) {
+        return 'ha de ser menor que ' + aurelia_validation_1.Utilities.getValue(threshold);
+      },
+      'NumericValidationRule': function NumericValidationRule(newValue, threshold) {
+        return 'ha de ser un nombre';
+      },
+      'NoSpacesValidationRule': function NoSpacesValidationRule(newValue, threshold) {
+        return 'no pot contenit espais';
+      },
+      'RegexValidationRule': function RegexValidationRule(newValue, threshold) {
+        return 'no és un valor vàlid';
+      },
+      'ContainsOnlyValidationRule': function ContainsOnlyValidationRule(newValue, threshold) {
+        return 'no és un valor vàlid';
+      },
+      'StrongPasswordValidationRule': function StrongPasswordValidationRule(newValue, threshold) {
+        return 'ha de contenir una combinació de lletres minúscules, majúscules, números i caràcters especials';
+      },
+      'MediumPasswordValidationRule': function MediumPasswordValidationRule(newValue, threshold) {
+        return 'ha de contenir almenys ' + aurelia_validation_1.Utilities.getValue(threshold) + ' dels següents grups: lletres minúscules, majúscules, números o caràcters especials';
+      },
+      'URLValidationRule': function URLValidationRule(newValue, threshold) {
+        return 'no és una URL vàlida';
+      }
+    }
+  };
+  exports.data = data;
+});
