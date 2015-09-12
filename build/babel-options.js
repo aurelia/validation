c23b49dfada8c7e8dd62abda77a1cb529e18efb7
Jeroen <Jeroenvinke17@gmail.com>
2015-09-12 10:48:48 +0200

    feat(typescript): export typescript definitions

--- build/babel-options.js
+++ build/babel-options.js
@@ -1,10 +1,14 @@
+var path = require('path');
+var paths = require('./paths');
+
 module.exports = {
   filename: '',
   filenameRelative: '',
+  modules: '',
   sourceMap: true,
   sourceMapName: '',
   sourceRoot: '',
-  moduleRoot: '',
+  moduleRoot: path.resolve('src').replace(/\\/g, '/'),
   moduleIds: false,
   experimental: false,
   comments: false,
@@ -16,5 +20,17 @@ module.exports = {
   optional: [
     "es7.decorators",
     "es7.classProperties"
-  ]
+  ],
+  plugins: [
+    "babel-dts-generator"
+  ],
+  extra: {
+    dts: {
+      packageName: paths.packageName,
+      typings: 'dist/',
+      suppressModulePath: true,
+      suppressComments: false,
+      memberOutputFilter: /^_.*/
+    }
+  }
 };
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -4,6 +4,32 @@ var to5 = require('gulp-babel');
 var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
+var through2 = require('through2');
+var concat = require('gulp-concat');
+var insert = require('gulp-insert');
+var rename = require('gulp-rename');
+var tools = require('aurelia-tools');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
+
+var jsName = paths.packageName + '.js';
+
+gulp.task('build-index', function(){
+  var importsToAdd = [];
+
+  return gulp.src([paths.root + '*.js', '!' + paths.root + 'index.js'])
+    .pipe(tools.sortFiles())
+    .pipe(through2.obj(function(file, enc, callback) {
+      file.contents = new Buffer(tools.extractImports(file.contents.toString("utf8"), importsToAdd));
+      this.push(file);
+      return callback();
+    }))
+    .pipe(concat(jsName))
+    .pipe(insert.transform(function(contents) {
+      return tools.createImportBlock(importsToAdd) + contents;
+    }))
+    .pipe(to5(assign({}, compilerOptions, {modules:'common'})));
+});
 
 gulp.task('build-es6', function () {
   return gulp.src(paths.source)
@@ -12,26 +38,45 @@ gulp.task('build-es6', function () {
 
 gulp.task('build-commonjs', function () {
   return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'common'})))
+    .pipe(to5(assign({}, compilerOptions, {modules:'common'}, {plugins: []})))
     .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 
 gulp.task('build-amd', function () {
   return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'amd'})))
+    .pipe(to5(assign({}, compilerOptions, {modules:'amd'}, {plugins: []})))
     .pipe(gulp.dest(paths.output + 'amd'));
 });
 
 gulp.task('build-system', function () {
   return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions, {modules:'system'})))
+    .pipe(to5(assign({}, compilerOptions, {modules:'system'}, {plugins: []})))
     .pipe(gulp.dest(paths.output + 'system'));
+});
+
+gulp.task('build-dts', function(){
+  var tdsPath = paths.output + paths.packageName + '/' + paths.packageName + '.d.ts';
+  return gulp.src(tdsPath)
+      .pipe(rename(paths.packageName + '.d.ts'))
+      .pipe(gulp.dest(paths.output + 'es6'))
+      .pipe(gulp.dest(paths.output + 'commonjs'))
+      .pipe(gulp.dest(paths.output + 'amd'))
+      .pipe(gulp.dest(paths.output + 'system'));
+});
+
+gulp.task('remove-dts-folder', function() {
+    var tdsFolder = paths.output + paths.packageName;
+    return gulp.src([tdsFolder])
+      .pipe(vinylPaths(del));
 });
 
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
     ['build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    'build-index',
+    'build-dts',
+    'remove-dts-folder',
     callback
   );
 });
