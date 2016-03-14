b0f2f1ea102c00426b841996da298f65950e96e0
Martyn Janes <martyn@obany.com>
2016-03-14 15:31:27 +0000

    fix(local): Use requirejs if available

--- src/validation-locale.js
+++ src/validation-locale.js
@@ -55,10 +55,17 @@ class ValidationLocaleRepository  {
         let locale = this.instances.get(localeIdentifier);
         resolve(locale);
       } else {
-        System.import(basePath + localeIdentifier).then((resource) => {
-          let locale = this.addLocale(localeIdentifier, resource.data);
-          resolve(locale);
-        });
+        if (window.require) {
+          require([basePath + localeIdentifier], function (resource) {
+            var locale = _this.addLocale(localeIdentifier, resource.data);
+            resolve(locale);
+          });
+        } else {
+          System['import'](basePath + localeIdentifier).then(function (resource) {
+            var locale = _this.addLocale(localeIdentifier, resource.data);
+            resolve(locale);
+          });
+        }
       }
     });
   }
