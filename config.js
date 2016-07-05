f1e0da62fe9a44543351ea83c117db7bd297517b
Rob Eisenberg <rob@bluespire.com>
2016-07-05 14:23:07 -0400

    chore(config): remove old babel config

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
 System.config({
   defaultJSExtensions: true,
-  transpiler: "babel",
-  babelOptions: {
-    "optional": [
-      "runtime",
-      "optimisation.modules.system"
-    ]
-  },
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
