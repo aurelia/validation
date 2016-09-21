41636e2451fccc06a016d0596fcafe63193231d8
dkent600 <github@douglaskent.com>
2016-09-21 12:23:57 -0400

    Add Validator parameter to ValidationController.createForCurrentScope
    It is not currently possible to provide a Validater to a specific
    instance of ValidationController, at least, not without using injection.
    
    ValidationController.createForCurrentScope has been provided to enable
    us to instantiate ValidationController without the use of injection, but
    provides no means of supplying a Validator for the controller instance.
    
    I have a use case where the ValidationController needs to be
    instantiated without the use of injection because the code is in a base
    class where, due to javascript/typescript limitations, injection cannot
    be used.
    
    Note: I'm not altering the documentation because it ties into:
    
    1) documentation about configuring the plugin, which I believe is
    missing altogether
    2) other means of assigning a Validator to a controller (through
    injection) that I understand are being added but may yet have been
    documented.
    
    Thus I think someone with more broad knowledge of all of this should
    undertake the task of documenting the whole thing together.

--- test/validation-controller-factory.ts
+++ test/validation-controller-factory.ts
@@ -1,7 +1,8 @@
 import {Container, Optional} from 'aurelia-dependency-injection';
 import {
   ValidationControllerFactory,
-  ValidationController
+  ValidationController,
+  Validator
 } from '../src/aurelia-validation';
 
 describe('ValidationControllerFactory', () => {
@@ -12,5 +13,17 @@ describe('ValidationControllerFactory', () => {
     const controller = factory.createForCurrentScope();
     expect(container.get(Optional.of(ValidationController))).toBe(null);
     expect(childContainer.get(Optional.of(ValidationController))).toBe(controller);
+  });
+
+  it('createForCurrentScopeWithValidator', () => {
+    const container = new Container();
+    const childContainer = container.createChild();
+
+    const factory = childContainer.get(ValidationControllerFactory);
+    const validator = childContainer.get(Validator);
+
+    const controller = factory.createForCurrentScope(validator);
+
+    expect(controller.validator).toBe(validator);
   });
 });
