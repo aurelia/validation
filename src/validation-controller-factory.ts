70b1176add59b8cfa940a97f33618d9ab7a3ee3d
Jeremy Danyow <jdanyow@gmail.com>
2016-09-25 21:00:00 -0400

    feat(ValidationControllerFactory): add validator arg to createForCurrentScope
    fixes #347

--- src/validation-controller-factory.ts
+++ src/validation-controller-factory.ts
@@ -1,5 +1,6 @@
 import {Container} from 'aurelia-dependency-injection';
 import {ValidationController} from './validation-controller';
+import {Validator} from './validator';
 
 /**
  * Creates ValidationController instances.
@@ -12,19 +13,21 @@ export class ValidationControllerFactory {
   constructor(private container: Container) {}
 
   /**
-   * Creates a new controller and registers it in the current element's container so that it's 
-   * available to the validate binding behavior and renderers.
+   * Creates a new controller instance.
    */
-  create() {
-    return this.container.invoke(ValidationController);
+  create(validator?: Validator) {
+    if (!validator) {
+      validator = <Validator>this.container.get(Validator);
+    }
+    return new ValidationController(validator);
   }
 
   /**
    * Creates a new controller and registers it in the current element's container so that it's 
    * available to the validate binding behavior and renderers.
    */
-  createForCurrentScope() {
-    const controller = this.create();
+  createForCurrentScope(validator?: Validator) {
+    const controller = this.create(validator);
     this.container.registerInstance(ValidationController, controller);
     return controller;
   }
