bea1cd79b11f0c1954873974182756adf8dcfec0
Jeremy Danyow <jdanyow@gmail.com>
2016-10-12 23:39:54 -0400

    fix(build): fix api.json generation
    related to #357

--- doc/shape-defs.js
+++ doc/shape-defs.js
@@ -9,6 +9,22 @@ try {
   const dtsPath = path.resolve(__dirname, `../dist/doc-temp/${packageName}.d.ts`);
   let defs = fs.readFileSync(dtsPath).toString();
 
+  // aggregate external imports
+  const packages = {};
+  const importRegex = /^\s*import\s+\{([^}]+)\}\s*from\s*'([\w|-]+)'/gm;
+  let importMatch  = importRegex.exec(defs);
+  while (importMatch) {
+    const packageName = importMatch[2];
+    const imports = packages[packageName] || (packages[packageName] = []);
+    const bindings = importMatch[1].split(',').map(x => x.trim());
+    for (let binding of bindings) {
+      if (imports.indexOf(binding) === -1) {
+        imports.push(binding);
+      }
+    }
+    importMatch = importRegex.exec(defs);
+  }
+
   // remove leading declare module
   defs = defs.replace(/^declare module ".*" \{/, '');
   // remove "} declare module {"
@@ -19,6 +35,14 @@ try {
   defs = defs.replace(/^\s+import.*;$/gm, '');
   // remove "export *"
   defs = defs.replace(/^\s+export \*.*;$/gm, '');
+
+  // write imports
+  for (let packageName in packages) {
+    if (packages.hasOwnProperty(packageName)) {
+      const imports = packages[packageName];
+      defs = `import {${imports.sort()}} from '${packageName}';\n` + defs;
+    }
+  }
 
   fs.writeFileSync(dtsPath, defs);
   console.log(`Shaped the dist/doc-temp/${packageName}.d.ts file.`);
